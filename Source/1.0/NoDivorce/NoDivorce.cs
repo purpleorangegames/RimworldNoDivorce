66da329f2ace3ccbfbf4834e53e61d4fdc681080
PurpleOrangeGames <tiago.carvalho@purpleorangegames.com>
2020-02-21 12:17:32 -0300

    Add files via upload

---
+++ Source/1.0/NoDivorce/NoDivorce.cs
@@ -0,0 +1,39 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Xml;
+using Verse;
+
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Verse.Steam;
+
+namespace NoDivorce
+{
+    [StaticConstructorOnStartup]
+    class NoDivorce : Mod
+    {
+        public NoDivorce(ModContentPack content) : base(content)
+        {
+            var harmony = HarmonyInstance.Create("net.pog.rimworld.mod.nodivorce");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+    }
+
+    [HarmonyPatch(typeof(InteractionWorker_Breakup))]
+    [HarmonyPatch(nameof(InteractionWorker_Breakup.RandomSelectionWeight))]
+    class Patch
+    {
+        static bool Prefix(ref float __result)
+        {
+            //Log.Error("Is it running?", false);
+			__result = 0f;
+            return false;
+        }
+    }
+
+}
